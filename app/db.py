f3a1f160ad5d1af065cb4216a7e0b7ab97ddfdc5
rahuljoshi1814 <136415480+rahuljoshi1814@users.noreply.github.com>
2025-05-25 11:16:36 +0530

    Update db.py

--- app/db.py
+++ app/db.py
@@ -1,10 +1,20 @@
 import psycopg2
+import os
+from urllib.parse import urlparse
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
 
 def get_connection():
+    database_url = os.getenv("DATABASE_URL")
+    if not database_url:
+        raise ValueError("DATABASE_URL environment variable not set")
+    result = urlparse(database_url)
     return psycopg2.connect(
-        dbname="Rj database",
-        user="postgres",
-        password="Rahul@2309",
-        host="localhost",
-        port="5432"
+        dbname= result.path[1:],
+        user=result.username,
+        password=result.password,
+        host=result.hostname,
+        port=result.port
     )
